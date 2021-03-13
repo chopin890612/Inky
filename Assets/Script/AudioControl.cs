74ef69a9c92d9a157b515ea53e0f62793a35d848
chopin890612 <54427752+chopin890612@users.noreply.github.com>
2021-03-13 16:18:07 +0800

    Revert "fix"
    This reverts commit b89935ecc008fd7dabc34eb331f2b9ca30e485df.

---
+++ Assets/Script/AudioControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControl : MonoBehaviour
+{
+    public AudioClip[] myAudio;
+    public void PlayApply()
+    {
+        GetComponent<AudioSource>().clip = myAudio[1];
+        GetComponent<AudioSource>().Play();
+    }
+    public void PlaySelect()
+    {
+        GetComponent<AudioSource>().clip = myAudio[0];
+        GetComponent<AudioSource>().Play();
+    }
+    public void PlayBack()
+    {
+        GetComponent<AudioSource>().clip = myAudio[2];
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ Assets/Script/New Systems/InputSystem.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class InputSystem : IGameSystemMono, IInputBehavior, IQuickDoubleTap
+{
+    private float beginTime;
+    private float quickTapTime = 0.3f;
+    private float quickTapPressTime = 0.3f;
+    private bool doubleTap = false;
+
+    public UnityEvent InputBeginEvent = new UnityEvent();
+    public UnityEvent InputPressEvent = new UnityEvent();
+    public UnityEvent InputEndEvent = new UnityEvent();
+    public UnityEvent QuickDoubleTapEvent = new UnityEvent();
+    public UnityEvent QuickDoubleTapPressEvent = new UnityEvent();
+
+    public override void StartGameSystem() { }
+    public override void DestoryGameSystem() { }
+
+    public bool InputBegin() { return Input.GetMouseButtonDown(0); }
+    public bool InputPress() { return Input.GetMouseButton(0); }
+    public bool InputEnd() { return Input.GetMouseButtonUp(0); }
+    public bool QuickDoubleTap()
+    {
+        if (Time.time - beginTime < quickTapTime)
+        {
+            doubleTap = true;
+            return true;
+        }
+        return false;
+    }
+    public bool QuickDoubleTapPress()
+    {
+        if (doubleTap)
+        {
+            if (Time.time - beginTime > quickTapPressTime)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void Update()
+    {
+        if (InputBegin())
+        {
+            InputBeginEvent.Invoke();
+
+            if (QuickDoubleTap())
+            {
+                QuickDoubleTapEvent.Invoke();
+            }
+            beginTime = Time.time;
+        }            
+        else if (InputPress())
+        {
+            InputPressEvent.Invoke();
+
+            if (QuickDoubleTapPress())
+            {
+                QuickDoubleTapPressEvent.Invoke();
+            }
+        }            
+        else if (InputEnd())
+        {
+            InputEndEvent.Invoke();
+
+            if (doubleTap)
+                doubleTap = false;
+        }
+    }
+}
+public interface IInputBehavior
+{
+    bool InputBegin();
+    bool InputPress();
+    bool InputEnd();
+}
+public interface IQuickDoubleTap
+{
+    bool QuickDoubleTap();
+    bool QuickDoubleTapPress();
+}
